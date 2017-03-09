52015664f783bb7ac3a86237778e40ad28a9e3b8
MikeB <burton.mj@gmail.com>
2017-03-08 18:00:14 -0700

    done for the second time

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 
+var express = require('express');
+var bodyParser = require('body-parser');
+var dateFormat = require('dateformat');
+
+var app = express();
+
+var port = 3000;
+
+app.listen(port, function(){
+  console.log('now listening on ' + port);
+})
+
+app.use(express.static('assets'))
+app.use(bodyParser.json())
+
+var messages = [{
+  message : "hello world",
+}];
+
+app.get('/messages', function(req, res, next){
+  res.status(200).json({
+    messages : messages
+  });
+})
+
+app.post('/messages', function(req, res, next){
+  var now = dateFormat(new Date(), "h:MM TT");
+
+  messages.push({message: req.body.message, time: now});
+  res.status(200).json({
+    messages : messages
+  });
+})
